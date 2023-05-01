6b32949107c2dcd983775c4398e7779d7716658a
nivalf <flavinleejohn@gmail.com>
2023-05-01 01:01:18 +0100

    gmapping using turtlebot3_slam added

--- src/find_free_space_server.py
+++ src/find_free_space_server.py
@@ -34,85 +34,89 @@ class FindFreeSpaceActionServer():
 
     # The action's "callback function":
     def action_server_launcher(self, goal: FindFreeSpaceGoal):
-        rate = rospy.Rate(10)
+        try:
+            rate = rospy.Rate(10)
 
-        ang_velocity = goal.ang_velocity
-        min_clear_distance = goal.min_clear_distance
+            ang_velocity = goal.ang_velocity
+            min_clear_distance = goal.min_clear_distance
 
 
-        if not self.is_req_valid(goal):
-            # abort the action server if an invalid goal has been requested...
-            # DEV: Add a message. Figure out how to do this.
-            self.actionserver.set_aborted()
-            return
+            if not self.is_req_valid(goal):
+                # abort the action server if an invalid goal has been requested...
+                # DEV: Add a message. Figure out how to do this.
+                self.actionserver.set_aborted()
+                return
 
-        # TODO: Print a message to indicate that the requested goal was valid
-        print(f"\n#####\n"
-            f"The 'find_free_space_action_server' has been called.\n"
-            f"Goal: Find free space with mininum free space of {min_clear_distance:.2f}m while turning with an angular velocity of {ang_velocity:.2f} rad/s...\n\n"
-            f"Commencing the action...\n"
-            f"#####\n")
-        
-        # Flag for status of the action
-        success = True
+            # TODO: Print a message to indicate that the requested goal was valid
+            print(f"\n#####\n"
+                f"The 'find_free_space_action_server' has been called.\n"
+                f"Goal: Find free space with mininum free space of {min_clear_distance:.2f}m while turning with an angular velocity of {ang_velocity:.2f} rad/s...\n\n"
+                f"Commencing the action...\n"
+                f"#####\n")
+            
+            # Flag for status of the action
+            success = True
 
-        # Get the robot's current odometry from the Tb3Odometry() class:
-        self.posx0 = self.tb3_odom.posx
-        self.posy0 = self.tb3_odom.posy
-        # Get information about objects up ahead from the Tb3LaserScan() class:
-        self.closest_object = self.tb3_lidar.min_distance
-        self.closest_object_location = self.tb3_lidar.closest_object_position
-
-        # set the robot's angular velocity (as specified in the "goal")...
-        self.vel_controller.set_move_cmd(0, ang_velocity)
-
-        # establish a conditional statement so that the
-        # while loop continues as long as the distance to the closest object
-        # ahead of the robot is always less than the "minimum clear distance"
-        # (as specified in the "goal")...
-        while self.tb3_lidar.min_distance < min_clear_distance:
-            # update LaserScan data:
+            # Get the robot's current odometry from the Tb3Odometry() class:
+            self.posx0 = self.tb3_odom.posx
+            self.posy0 = self.tb3_odom.posy
+            # Get information about objects up ahead from the Tb3LaserScan() class:
             self.closest_object = self.tb3_lidar.min_distance
             self.closest_object_location = self.tb3_lidar.closest_object_position
 
-            # publish a velocity command to make the robot start turning
-            self.vel_controller.publish()
+            # set the robot's angular velocity (as specified in the "goal")...
+            self.vel_controller.set_move_cmd(0, ang_velocity)
 
-            # check if there has been a request to cancel the action mid-way through:
-            if self.actionserver.is_preempt_requested():
-                # take appropriate action if the action is cancelled (peempted)...
-                rospy.loginfo("Cancelling the Search for free space.")
-              
-                # DEV: Add a message. Figure out how to do this.
-                self.actionserver.set_preempted()
-                # stop the robot:
+            # establish a conditional statement so that the
+            # while loop continues as long as the distance to the closest object
+            # ahead of the robot is always less than the "minimum clear distance"
+            # (as specified in the "goal")...
+            while self.tb3_lidar.min_distance < min_clear_distance:
+                # update LaserScan data:
+                self.closest_object = self.tb3_lidar.min_distance
+                self.closest_object_location = self.tb3_lidar.closest_object_position
+
+                # publish a velocity command to make the robot start turning
+                self.vel_controller.publish()
+
+                # check if there has been a request to cancel the action mid-way through:
+                if self.actionserver.is_preempt_requested():
+                    # take appropriate action if the action is cancelled (peempted)...
+                    rospy.loginfo("Cancelling the Search for free space.")
+                
+                    # DEV: Add a message. Figure out how to do this.
+                    self.actionserver.set_preempted()
+                    # stop the robot:
+                    self.vel_controller.stop()
+                    success = False
+                    # exit the loop:
+                    break
+
+                # determine how far the robot has travelled so far:
+                # DEV: change to find the angle turned
+                self.distance = sqrt(
+                    pow(self.posx0 - self.tb3_odom.posx, 2) + pow(self.posy0 - self.tb3_odom.posy, 2))
+
+                # DEV TODO: update all feedback message values and publish a feedback message:
+                self.feedback.current_angle_turned = 0
+                self.actionserver.publish_feedback(self.feedback)
+
+
+                rate.sleep()
+
+            if success:
+                rospy.loginfo("Found Free Space")
+
+                # DEV TODO: update all result parameters:
+                self.result.success = success
+                self.result.turned_angle = 0
+
+                # TODO: Set the action server to "succeeded" and stop the robot...
+                self.actionserver.set_succeeded(self.result)
                 self.vel_controller.stop()
-                success = False
-                # exit the loop:
-                break
-
-            # determine how far the robot has travelled so far:
-            # DEV: change to find the angle turned
-            self.distance = sqrt(
-                pow(self.posx0 - self.tb3_odom.posx, 2) + pow(self.posy0 - self.tb3_odom.posy, 2))
-
-            # DEV TODO: update all feedback message values and publish a feedback message:
-            self.feedback.current_angle_turned = 0
-            self.actionserver.publish_feedback(self.feedback)
-
-
-            rate.sleep()
-
-        if success:
-            rospy.loginfo("Found Free Space")
-
-            # DEV TODO: update all result parameters:
-            self.result.success = success
-            self.result.turned_angle = 0
-
-            # TODO: Set the action server to "succeeded" and stop the robot...
-            self.actionserver.set_succeeded(self.result)
-            self.vel_controller.stop()
+                
+        except rospy.ROSInterruptException:
+            pass
 
     # Handle invalid requests
     def is_req_valid(self, goal: FindFreeSpaceGoal):
@@ -143,3 +147,4 @@ if __name__ == '__main__':
     rospy.init_node("find_free_space_action_server")
     FindFreeSpaceActionServer()
     rospy.spin()
+
--- src/search_server.py
+++ src/search_server.py
@@ -34,83 +34,86 @@ class SearchActionServer():
 
     # The action's "callback function":
     def action_server_launcher(self, goal: SearchGoal):
-        rate = rospy.Rate(10)
+        try:
+            rate = rospy.Rate(10)
 
-        fwd_velocity = goal.fwd_velocity
-        approach_distance = goal.approach_distance
+            fwd_velocity = goal.fwd_velocity
+            approach_distance = goal.approach_distance
 
 
-        if not self.is_req_valid(goal):
-            # abort the action server if an invalid goal has been requested...
-            # DEV: Add a message. Figure out how to do this.
-            self.actionserver.set_aborted()
-            return
+            if not self.is_req_valid(goal):
+                # abort the action server if an invalid goal has been requested...
+                # DEV: Add a message. Figure out how to do this.
+                self.actionserver.set_aborted()
+                return
 
-        # TODO: Print a message to indicate that the requested goal was valid
-        print(f"\n#####\n"
-            f"The 'search_action_server' has been called.\n"
-            f"Goal: Move forward with {fwd_velocity:.2f} m/s velocity and stop at {approach_distance:.2f} distance from the object ...\n\n"
-            f"Commencing the action...\n"
-            f"#####\n")
-        
-        # Flag for status of the action
-        success = True
+            # TODO: Print a message to indicate that the requested goal was valid
+            print(f"\n#####\n"
+                f"The 'search_action_server' has been called.\n"
+                f"Goal: Move forward with {fwd_velocity:.2f} m/s velocity and stop at {approach_distance:.2f} distance from the object ...\n\n"
+                f"Commencing the action...\n"
+                f"#####\n")
+            
+            # Flag for status of the action
+            success = True
 
-        # Get the robot's current odometry from the Tb3Odometry() class:
-        self.posx0 = self.tb3_odom.posx
-        self.posy0 = self.tb3_odom.posy
-        # Get information about objects up ahead from the Tb3LaserScan() class:
-        self.closest_object = self.tb3_lidar.min_distance
-        self.closest_object_location = self.tb3_lidar.closest_object_position
-
-        # TODO: set the robot's forward velocity (as specified in the "goal")...
-        self.vel_controller.set_move_cmd(fwd_velocity)
-
-        # TODO: establish a conditional statement so that the
-        # while loop continues as long as the distance to the closest object
-        # ahead of the robot is always greater than the "approach distance"
-        # (as specified in the "goal")...
-        while self.tb3_lidar.min_distance > approach_distance:
-            # update LaserScan data:
+            # Get the robot's current odometry from the Tb3Odometry() class:
+            self.posx0 = self.tb3_odom.posx
+            self.posy0 = self.tb3_odom.posy
+            # Get information about objects up ahead from the Tb3LaserScan() class:
             self.closest_object = self.tb3_lidar.min_distance
             self.closest_object_location = self.tb3_lidar.closest_object_position
 
-            # publish a velocity command to make the robot start moving
-            self.vel_controller.publish()
+            # TODO: set the robot's forward velocity (as specified in the "goal")...
+            self.vel_controller.set_move_cmd(fwd_velocity)
 
-            # check if there has been a request to cancel the action mid-way through:
-            if self.actionserver.is_preempt_requested():
-                # TODO: take appropriate action if the action is cancelled (peempted)...
-                rospy.loginfo("Cancelling the search.")
-              
-                # DEV: Add a message. Figure out how to do this.
-                self.actionserver.set_preempted()
-                # stop the robot:
+            # TODO: establish a conditional statement so that the
+            # while loop continues as long as the distance to the closest object
+            # ahead of the robot is always greater than the "approach distance"
+            # (as specified in the "goal")...
+            while self.tb3_lidar.min_distance > approach_distance:
+                # update LaserScan data:
+                self.closest_object = self.tb3_lidar.min_distance
+                self.closest_object_location = self.tb3_lidar.closest_object_position
+
+                # publish a velocity command to make the robot start moving
+                self.vel_controller.publish()
+
+                # check if there has been a request to cancel the action mid-way through:
+                if self.actionserver.is_preempt_requested():
+                    # TODO: take appropriate action if the action is cancelled (peempted)...
+                    rospy.loginfo("Cancelling the search.")
+                
+                    # DEV: Add a message. Figure out how to do this.
+                    self.actionserver.set_preempted()
+                    # stop the robot:
+                    self.vel_controller.stop()
+                    success = False
+                    # exit the loop:
+                    break
+
+                # determine how far the robot has travelled so far:
+                self.distance = sqrt(
+                    pow(self.posx0 - self.tb3_odom.posx, 2) + pow(self.posy0 - self.tb3_odom.posy, 2))
+
+                # TODO: update all feedback message values and publish a feedback message:
+                self.feedback.current_distance_travelled = self.distance
+                self.actionserver.publish_feedback(self.feedback)
+
+                # TODO: update all result parameters:
+                self.result.total_distance_travelled = self.distance
+                self.result.closest_object_distance = self.closest_object
+                self.result.closest_object_angle = self.closest_object_location
+
+                rate.sleep()
+
+            if success:
+                rospy.loginfo("approach completed successfully.")
+                # TODO: Set the action server to "succeeded" and stop the robot...
+                self.actionserver.set_succeeded(self.result)
                 self.vel_controller.stop()
-                success = False
-                # exit the loop:
-                break
-
-            # determine how far the robot has travelled so far:
-            self.distance = sqrt(
-                pow(self.posx0 - self.tb3_odom.posx, 2) + pow(self.posy0 - self.tb3_odom.posy, 2))
-
-            # TODO: update all feedback message values and publish a feedback message:
-            self.feedback.current_distance_travelled = self.distance
-            self.actionserver.publish_feedback(self.feedback)
-
-            # TODO: update all result parameters:
-            self.result.total_distance_travelled = self.distance
-            self.result.closest_object_distance = self.closest_object
-            self.result.closest_object_angle = self.closest_object_location
-
-            rate.sleep()
-
-        if success:
-            rospy.loginfo("approach completed successfully.")
-            # TODO: Set the action server to "succeeded" and stop the robot...
-            self.actionserver.set_succeeded(self.result)
-            self.vel_controller.stop()
+        except rospy.ROSInterruptException:
+            pass
 
     # Handle invalid requests
     def is_req_valid(self, goal: SearchGoal):
